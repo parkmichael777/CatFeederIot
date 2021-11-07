f5b12d038340129e3052fa9cebb6be0787ea232e
Michael Park <parkmichael777@gmail.com>
2021-11-07 14:44:04 -0600

    Changes to data packet system. - Data no longer stored in cat profile. - Data no longer locked. - Instead, new data packet stores cat#, amt disp, and timestamp. - Data packets are sent to a queue (synced by kernel). - Changed portionGrams to a float and portionTimes to a uint64_t.

--- Arduino/CatFoodDropperFirmware/CatProfile.h
+++ Arduino/CatFoodDropperFirmware/CatProfile.h
@@ -5,30 +5,32 @@
 typedef struct __attribute__((__packed__)) {
     uint8_t inUse;                          // Bool if this profile is active.
     float maxRate;                          // Max rate in grams/min.
-    uint8_t portionGrams;                   // Weight dispensed in one portion.
+    float portionGrams;                     // Weight dispensed in one portion.
     uint8_t numPortions;                    // Number of active indices in portionTimes.
-    uint32_t portionTimes[NUM_PORTIONS];    // Time in minutes since 00:00 relative to CST.
+    TIME_T portionTimes[NUM_PORTIONS];      // Time in minutes since 00:00 relative to CST.
     uint64_t catID;                         // Holds 5 byte RFID tag number.
 } catProfileServer;
 
 /* Device Structs */
-typedef struct {
-    uint32_t amountDispensed;
-    uint32_t portionDuration;
-} bowlData;
+typedef struct __attribute__((__packed__)) {
+    int profileIndex;
+    float amountDispensed;
+    TIME_T timeStamp;
+} dataPacket;
 
 typedef struct __attribute__((__packed__)) {
     uint8_t inUse;                          // Bool if this profile is active.
     float maxRate;                          // Max rate in grams/min.
-    uint8_t portionGrams;                   // Weight dispensed in one portion.
+    float portionGrams;                     // Weight dispensed in one portion.
     uint8_t numPortions;                    // Number of active indices in portionTimes.
-    uint32_t portionTimes[NUM_PORTIONS];    // Time in minutes since 00:00 relative to CST.
+    TIME_T portionTimes[NUM_PORTIONS];      // Time in minutes since 00:00 relative to CST.
     uint64_t catID;                         // Holds 5 byte RFID tag number.
 
     // Visual aid:
     // Time axis |----Feeding Period----|---Waiting Period---|   |--Next Feeding Period--|--so on...
     //           ^StartISR              ^EndISR                  ^StartISR               ^EndISR
 
+    uint32_t amountDispensed;               // Grams of food dispensed for current portion.
     uint8_t canEat;                         // Indicates whether in feeding period.
     uint8_t inProgress;                     // Indicates whether cat started current feeding period.
     uint8_t isComplete;                     // Indicates whether cat finished alloted portion for
@@ -36,10 +38,6 @@ typedef struct __attribute__((__packed__)) {
 
     volatile uint8_t timeEINTR;             // Indicates an interrupt occurred.
     esp_timer_handle_t timerHandle;
-
-    volatile uint8_t dataFlag;              // Indicates bowlData is ready to send for this portion.
-    bowlData data;
-    SemaphoreHandle_t dataLock;
 } catProfile;
 
 #endif
