e5f15ffd518f026d00f400978a08ea7440bfa4a7
Michael Park <parkmichael777@gmail.com>
2021-11-16 10:25:40 -0600

    Added RFID, CELL, and MOTOR pin macros; Changed feed period to 1 hour

--- Arduino/CatFoodDropperFirmware/config.h
+++ Arduino/CatFoodDropperFirmware/config.h
@@ -5,12 +5,12 @@
 #define NUM_CATS      3               // Max num of supported cats. 
                                       // Changing NUM_CATS requires manual additions to intrArgs :(
 #define NUM_PORTIONS  5               // Max num of schedulable portions.
-#define FEED_PERIOD   1 * (60000)     // Number of ms portion is available for after it starts.
+#define FEED_PERIOD   60 * (60000)    // Number of ms portion is available for after it starts.
 #define POLL_PERIOD   60 * (1000000)  // Number of us until bowl polls server for catProfile updates.
 #define DISP_PERIOD   60 * (1000000)  // Number of us each dispensing period within a portion lasts.
 
 /* WiFi and Server Config */
-#define EDU_WIFI      0               // Whether we are connecting to IllinoisNet or not.
+#define EDU_WIFI      1               // Whether we are connecting to IllinoisNet or not.
 
 #if EDU_WIFI
 #define SSID          "IllinoisNet_Guest"
@@ -23,27 +23,30 @@
 #define SERVER_PORT   8000
 #endif
 
+/* Debug Config */
+#define DEBUG_MODE    1               // Enable/Disable extra logging.
+#define VERBOSE_MODE  0               // Enable/Disable extra extra logging.
+
 /* Constants */
 #define DAY           86400000        // Number of milliseconds in a day.
 #define RFID_DATA     10              // Number of data bytes in RFID packet
 
 /* Hardware Pins */
-#define RX_PIN        16              // UART RX/TX
+#define RX_PIN        27              // UART RX/TX
 #define TX_PIN        17
 
-#define LED           13              // Debug LED
+#define RFID_NEARBY   26              // RFID
 
-#define RFID_NEARBY   21              // RFID
+#define CELL_DOUT     19              // Load cell
+#define CELL_SCLK     21 
+#define CELL_PWDN     18
+#define CELL_SPEED    23
+#define CELL_CALIB    -5990.0        // Tuned via a known control weight.
+#define CELL_READS    3              // Number of readings to average in order to return one weight.
 
-#define CELL_DOUT     23              // Load cell
-#define CELL_SCLK     22 
-#define CELL_PWDN     14
-#define CELL_SPEED    32
-#define CELL_CALIB    -7050.0         // Default provided by Sparkfun; TODO: tune to 1g.
-
-/* Debug Config */
-#define DEBUG_MODE    1               // Enable/Disable extra logging.
-#define VERBOSE_MODE  1               // Enable/Disable extra extra logging.
+#define MOTOR_STBY    14
+#define MOTOR_IN1     12
+#define MOTOR_IN2     13
 
 /* Types */
 typedef uint64_t TIME_T;
